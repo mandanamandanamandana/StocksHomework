7ffedb0431c2f78f4d6ff8511a795b44a2e60f70
Mandana <mandy7373@gmail.com>
2023-01-11 22:11:43 -0500

    please

--- src/loaders.js
+++ src/loaders.js
@@ -3,4 +3,4 @@ export const priceLoader = async({params}) => {
     const apiKey = "224d3ba0da953f4ed257315733c21ee9"
 
     return (await fetch(`https://financialmodelingprep.com/api/v3/stock-price-change/${symbol}?apikey=${apiKey}`)
-}
+)}
